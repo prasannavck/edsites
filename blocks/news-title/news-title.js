837cd6ff4f9add7173e601f1408ab7b555e94ab3
JiangLong2019 <57093988+JiangLong2019@users.noreply.github.com>
2024-09-19 14:44:00 +0900

    UE - News Title (#87)

--- blocks/news-title/news-title.js
+++ blocks/news-title/news-title.js
@@ -1,49 +1,60 @@
-import { readBlockConfig } from '../../scripts/aem.js';
 import { createElement } from '../../scripts/blocks-utils.js';
 
-function createNewsTitle(block) {
+const DROPDOWN_ICON_GREEN = `${window.hlx.codeBasePath}/icons/chevron-down-green.svg`;
+const DROPDOWN_ICON_WHITE = `${window.hlx.codeBasePath}/icons/chevron-down-white.svg`;
+
+function createNewsTitle(content) {
   const titleDiv = createElement('div', 'left-title');
-  const title = block.querySelector('h1');
-  titleDiv.appendChild(title);
+  titleDiv.appendChild(content[0]?.firstElementChild);
   return titleDiv;
 }
 
-function getDropdownTitle(block) {
-  const blockCfg = readBlockConfig(block);
-  const buttonTitle = blockCfg['dropdown-title'];
-  return buttonTitle;
-}
-
-function clickEvent(button, ul) {
+function clickEvent(button, ul, chevronIcon) {
   button.addEventListener('click', () => {
     ul.classList.toggle('visible');
     button.classList.toggle('visible');
+    chevronIcon.src = `${DROPDOWN_ICON_WHITE}`;
   });
   // close dropdown list when click outside
   window.addEventListener('click', (e) => {
     if (!button.contains(e.target) && !ul.contains(e.target)) {
       ul.classList.remove('visible');
       button.classList.remove('visible');
+      chevronIcon.src = `${DROPDOWN_ICON_GREEN}`;
+    }
+  });
+  // change icon by mouseover event
+  button.addEventListener('mouseover', () => {
+    chevronIcon.src = `${DROPDOWN_ICON_WHITE}`;
+  });
+  button.addEventListener('mouseleave', () => {
+    if (!button.classList.contains('visible')) {
+      chevronIcon.src = `${DROPDOWN_ICON_GREEN}`;
     }
   });
 }
 
-function createCategoryMenu(block) {
+function createCategoryMenu(block, content) {
   const categoryDropdown = createElement('div', 'right-dropdown');
   const categoryButton = createElement('button', 'category-button');
   const buttonTitle = createElement('span', 'button-title');
-  buttonTitle.textContent = getDropdownTitle(block);
+  buttonTitle.textContent = content[1]?.textContent;
+  const chevronIcon = createElement('img', 'dropdown-icon');
+  chevronIcon.src = `${DROPDOWN_ICON_GREEN}`;
+  chevronIcon.alt = 'Dropdown chevron icon';
   categoryButton.appendChild(buttonTitle);
+  categoryButton.appendChild(chevronIcon);
   const dropdownList = block.querySelector('ul');
-  clickEvent(categoryButton, dropdownList);
+  clickEvent(categoryButton, dropdownList, chevronIcon);
   categoryDropdown.appendChild(categoryButton);
-  categoryDropdown.appendChild(dropdownList);
+  if (dropdownList) categoryDropdown.appendChild(dropdownList);
   return categoryDropdown;
 }
 
 export default async function decorate(block) {
-  const newsTitle = createNewsTitle(block);
-  const categoryMenu = createCategoryMenu(block);
+  const subContent = block.querySelectorAll(':scope > div > div');
+  const newsTitle = createNewsTitle(subContent);
+  const categoryMenu = createCategoryMenu(block, subContent);
   const container = createElement('div', 'news-title-container');
   container.append(newsTitle);
   container.append(categoryMenu);
