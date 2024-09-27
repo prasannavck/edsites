bc9c4c58ace578b91e22af943a1f59d5001a1520
nawaz-adobe <115994423+nawaz-adobe@users.noreply.github.com>
2024-09-27 15:55:28 +0530

    fix: migrate hero block variants to UE (#103)
    * fix: adding buttons stack definition
    
    * fix: support links
    
    * fix: migrate block to UE
    
    * fix: content model changes for coverage variant
    
    * fix: adding another button to support tabs
    
    * fix: add active tab option
    
    * fix: remove active tab config
    
    * fix: add gradient overlay option
    
    * fix: block refactoring for UE
    
    * fix: icon path with UE
    
    * fix: using tabs for buttons
    
    * fix: testing conditional property
    
    * fix: try advanced json logic
    
    * fix: passing required count filter
    
    * fix: json logic condition
    
    * fix: support list for GH icons
    
    * fix: lint issues
    
    * fix: clean up and instrumentation fix
    
    * fix: keeping tag for links
    
    * fix: reverting pTag changes
    
    * fix: new component model changes
    
    * fix: test logging
    
    * fix: instrumentation issue with header tags
    
    * fix: handle markup changes
    
    * fix: migrate variants to UE
    
    * fix: skipping lint errors as exceptions
    
    * fix: address review comments

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,64 +1,101 @@
-export default function decorate(block) {
-  const col = block.querySelector(':scope > div > div');
-  const h1 = col.querySelector('h1');
-  const h2 = col.querySelector('h2');
-  const h3 = col.querySelector('h3');
-  const allPTags = col.querySelectorAll('p');
-  const pTagsWithIcon = Array.from(allPTags).filter((p) => p.querySelector('.icon'));
+import { moveInstrumentation } from '../../scripts/scripts.js';
 
-  const backgroundImageContainer = block.querySelector(':scope > div > div > p');
+export default function decorate(block) {
+  const rows = block.querySelectorAll(':scope > div > div');
+  const content = rows[0];
+  const h1 = content.querySelector('h1');
+  const h2 = content.querySelector('h2');
+  const h3 = content.querySelector('h3');
+  const allPTags = content.querySelectorAll('p');
+  const pTagsWithPicture = Array.from(allPTags).filter((p) => p.querySelector('.icon'));
+  // eslint-disable-next-line arrow-body-style
+  const pTagsWithoutIconOrPicture = Array.from(allPTags).filter((p) => {
+    return !p.querySelector('.icon') && !p.querySelector('picture');
+  });
+
   if (block.classList.contains('background-gradient')) {
+    const backgroundImageContainer = content.querySelector('p:has(picture)');
     const gradientOverlay = document.createElement('div');
     gradientOverlay.classList.add('gradient-overlay');
     backgroundImageContainer.appendChild(gradientOverlay);
   }
 
-  pTagsWithIcon.forEach((p) => {
-    const textSpan = document.createElement('span');
-    textSpan.classList.add('icon-text');
-    Array.from(p.childNodes).forEach((node) => {
-      if (node.nodeType === Node.ELEMENT_NODE && node.classList.contains('icon')) {
-        // Skip the icon span
-        return;
-      }
-      textSpan.appendChild(node);
-    });
-    p.appendChild(textSpan);
-  });
+  if (block.classList.contains('coverage')) {
+    // Decorate heading
+    if (h1 && pTagsWithPicture[0] && pTagsWithPicture[1]) {
+      const icon1 = pTagsWithPicture[0].querySelector('.icon');
+      moveInstrumentation(pTagsWithPicture[0], icon1);
+      const icon2 = pTagsWithPicture[1].querySelector('.icon');
+      moveInstrumentation(pTagsWithPicture[1], icon2);
+      h1.prepend(icon2);
+      h1.prepend('+');
+      h1.prepend(icon1);
+      pTagsWithPicture[0].remove();
+      pTagsWithPicture[1].remove();
+    }
 
-  let activeTab = -1;
-  const activeTabs = Array.from(block.classList).find((className) => className.startsWith('active-'));
-  if (activeTabs) {
-    const val = activeTabs.split('-')[1];
-    activeTab = parseInt(val, 10) - 1;
+    // Decorate paragraph
+    if (pTagsWithoutIconOrPicture[1] && pTagsWithPicture[2]) {
+      const icon = pTagsWithPicture[2].querySelector('.icon');
+      moveInstrumentation(pTagsWithPicture[2], icon);
+      pTagsWithoutIconOrPicture[1].prepend(icon);
+      pTagsWithPicture[2].remove();
+    }
+
+    const pTagsWithIcon = Array.from(allPTags).filter((p) => p.querySelector('.icon'));
+    pTagsWithIcon.forEach((p) => {
+      const textSpan = document.createElement('span');
+      textSpan.classList.add('icon-text');
+      Array.from(p.childNodes).forEach((node) => {
+        if (node.nodeType === Node.ELEMENT_NODE && node.classList.contains('icon')) {
+          // Skip the icon span
+          return;
+        }
+        textSpan.appendChild(node);
+      });
+      p.appendChild(textSpan);
+      moveInstrumentation(p, textSpan);
+    });
   }
 
-  const ul = col.querySelector('ul');
-  const li = ul.querySelectorAll('li');
-  li.forEach((item, index) => {
-    const link = item.querySelector('a');
-    if (link) {
+  const activeTab = 3;
+  const ul = document.createElement('ul');
+  for (let i = 1; i < rows.length; i += 1) {
+    if (rows[i].childNodes.length > 0) {
+      const li = document.createElement('li');
+      const buttonContainer = rows[i].querySelector('.button-container');
+      const link = buttonContainer.querySelector('a');
+      const pWithIcon = rows[i].querySelector('p:has(.icon)');
+      if (pWithIcon) {
+        const iconSpan = pWithIcon.querySelector('span.icon');
+        moveInstrumentation(pWithIcon, iconSpan);
+        link.prepend(iconSpan);
+      }
       link.classList.add('button');
+
       if (block.classList.contains('buttons-stack')) {
-        if (index === 0) {
+        if (i === 1) {
           link.classList.add('orange');
         } else {
           link.classList.add('dark');
         }
-        const icon = item.querySelector('span.icon');
-        link.prepend(icon);
       }
-      if (block.classList.contains('tabs') && index === activeTab) {
-        item.classList.add('active');
+
+      if (block.classList.contains('tabs') && i === activeTab) {
+        li.classList.add('active');
       }
+
+      li.append(buttonContainer);
+      ul.appendChild(li);
     }
-  });
+    rows[i].parentNode.remove();
+  }
 
   if (block.classList.contains('tabs')) {
     const searchBar = document.createElement('li');
     searchBar.innerHTML = `
-    <img src="/images/icon-search-grey.png" />
-    <input type="text" id="search" placeholder="Search Articles">
+    <img src="${window.hlx.codeBasePath}/images/icon-search-grey.png" alt="Search" />
+    <input type="text" id="search" placeholder="Search Articles" >
     `;
     ul.appendChild(searchBar);
   }
@@ -67,6 +104,7 @@ export default function decorate(block) {
   heroMenu.classList.add('hero-menu');
   const heroContent = document.createElement('div');
   heroContent.classList.add('hero-content');
+
   if (h1) {
     heroContent.appendChild(h1);
   }
@@ -77,12 +115,11 @@ export default function decorate(block) {
     heroContent.appendChild(h3);
   }
 
-  const pTagsWithoutPicture = Array.from(allPTags).filter((p) => !p.querySelector('picture'));
-  for (let i = 0; i < pTagsWithoutPicture.length; i += 1) {
-    heroContent.appendChild(pTagsWithoutPicture[i]);
+  for (let i = 0; i < pTagsWithoutIconOrPicture.length; i += 1) {
+    heroContent.appendChild(pTagsWithoutIconOrPicture[i]);
   }
 
   heroMenu.appendChild(heroContent);
   heroMenu.appendChild(ul);
-  col.appendChild(heroMenu);
+  content.appendChild(heroMenu);
 }
