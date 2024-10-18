dc49878a962fe01c906e2cd0dc4b2d7c1cdfce12
JiangLong2019 <57093988+JiangLong2019@users.noreply.github.com>
2024-10-18 18:17:44 +0900

    FAQs - close all faq items by default  (#174)

--- blocks/faqs/faqs.js
+++ blocks/faqs/faqs.js
@@ -4,16 +4,12 @@ import { fetchPlaceholders } from '../../scripts/aem.js';
 
 function addEvent(faqTitle, placeholders) {
   faqTitle.addEventListener('click', () => {
-    faqTitle.classList.toggle('visible');
+    const faqItem = faqTitle.closest('.faq-item');
+    faqItem.classList.toggle('visible');
     const faqToggle = faqTitle.querySelector('.faq-title-right');
     if (faqToggle) {
-      faqToggle.classList.toggle('visible');
-      faqToggle.textContent = faqToggle.classList.contains('visible')
+      faqToggle.textContent = faqItem.classList.contains('visible')
         ? `${placeholders.hidedetail}` : `${placeholders.showdetail}`;
-    }
-    const faqContent = faqTitle.nextElementSibling;
-    if (faqContent) {
-      faqContent.classList.toggle('visible');
     }
   });
 }
@@ -21,7 +17,7 @@ function addEvent(faqTitle, placeholders) {
 export default async function decorate(block) {
   const placeholders = await fetchPlaceholders();
   const faqPanel = createElement('div', 'faq-panel');
-  [...block.children].forEach((row, index) => {
+  [...block.children].forEach((row) => {
     // decorate faq item label
     const label = row.children[0];
     const faqTitle = createElement('div', 'faq-title');
@@ -39,13 +35,6 @@ export default async function decorate(block) {
     const content = createElement('div', 'faq-content');
     contentPanel.append(contentWrapper);
     content.append(contentPanel);
-    // open first faq item by default
-    if (index === 0) {
-      faqTitle.classList.add('visible');
-      toggle.textContent = `${placeholders.hidedetail}`;
-      toggle.classList.add('visible');
-      content.classList.add('visible');
-    }
     const faqItem = createElement('div', 'faq-item');
     moveInstrumentation(row, faqItem);
     faqItem.append(faqTitle, content);
