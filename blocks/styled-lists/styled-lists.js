82a0451832d396516448387c14eb622c118bf327
Jacob <161779972+jacobrendina@users.noreply.github.com>
2024-10-31 08:58:42 +1100

    Enhancing styled lists with tooltip support, footnote support, and background colour options (#203)
    * [#145] Expanding styling options to cover all cases
    
    * [#145] Fixed model
    
    * [#145] Fixing display of footnote and teal boolean
    
    * [#145] Added tooltip functionality, finalising styles
    
    * [#145] Finetuning
    
    * [#145] Keeping lint happy
    
    * [#145] Simplifying PR
    
    * [#145] Added dark check icon option, updated styling based on comments

--- blocks/styled-lists/styled-lists.js
+++ blocks/styled-lists/styled-lists.js
@@ -1,4 +1,5 @@
 const SHOW_MORE_ICON = `${window.hlx.codeBasePath}/icons/arrow-circle-teal.svg`;
+const TOOLTIP_ICON = `${window.hlx.codeBasePath}/icons/question-teal.svg`;
 
 function handleShowMoreFunctionality(list, showMoreThreshold) {
   const listItems = list.querySelectorAll('li');
@@ -26,8 +27,25 @@ function handleShowMoreFunctionality(list, showMoreThreshold) {
   });
 }
 
+function handleTooltips(list) {
+  const tooltips = list.querySelectorAll('sup');
+  tooltips.forEach((tooltipText) => {
+    const tooltip = document.createElement('button');
+    tooltip.classList.add('icon', 'tooltip');
+    tooltipText.parentNode.insertBefore(tooltip, tooltipText);
+
+    tooltipText.classList.add('tooltip-content');
+    tooltip.append(tooltipText);
+
+    const tooltipIcon = document.createElement('img');
+    tooltipIcon.setAttribute('src', TOOLTIP_ICON);
+    tooltipIcon.setAttribute('alt', 'Tooltip');
+    tooltip.append(tooltipIcon);
+  });
+}
+
 export default function decorate(block) {
-  // handle custom bullet icon
+  // Custom bullet icon
   const bulletIcon = block.querySelector('.styled-lists > div:first-child span.icon > img');
   const iconSrc = bulletIcon.src;
   const listItems = block.querySelectorAll('ul > li');
@@ -36,17 +54,39 @@ export default function decorate(block) {
     listItem.style.backgroundSize = '1.25rem';
     listItem.style.paddingInlineStart = '1.8rem';
   });
-  block.querySelector('.styled-lists > div:first-child')?.remove(); // remove bullet icon div
+  block.querySelector('.styled-lists > div:first-child')?.remove();
 
-  // handle 'Show more' functionality
+  // Tooltips & show more button
   const showMoreThreshold = block.querySelector('.styled-lists > div:first-child p')?.textContent;
-  if (showMoreThreshold > 0) {
-    const lists = block.querySelectorAll('ul');
-    lists.forEach((list) => {
-      if (list.children.length > showMoreThreshold) {
-        handleShowMoreFunctionality(list, showMoreThreshold);
-      }
-    });
+  const lists = block.querySelectorAll('ul');
+  lists.forEach((list) => {
+    handleTooltips(list);
+    if (showMoreThreshold > 0 && list.children.length > showMoreThreshold) {
+      handleShowMoreFunctionality(list, showMoreThreshold);
+    }
+  });
+  block.querySelector('.styled-lists > div:first-child')?.remove();
+
+  // Lists footnote
+  const listsFootnote = block.querySelector('.styled-lists > div:first-child > div');
+  listsFootnote.classList.add('footnote');
+  block.append(listsFootnote);
+  block.querySelector('.styled-lists > div:first-child')?.remove();
+
+  // Teal background
+  const backgroundBool = block.querySelector('.styled-lists > div:first-child p')?.textContent;
+  if (backgroundBool === 'true') {
+    block.classList.add('teal-background');
+  } else {
+    block.classList.remove('teal-background');
   }
-  block.querySelector('.styled-lists > div:first-child')?.remove(); // remove showMoreThreshold div
+  block.querySelector('.styled-lists > div:first-child')?.remove();
+
+  // Style lists columns
+  const listCols = document.createElement('div');
+  listCols.classList.add('list-columns');
+  block.querySelectorAll('.styled-lists > div:not(.footnote').forEach((col) => {
+    listCols.append(col);
+  });
+  block.prepend(listCols);
 }
