7455e5b85917f682a1698c39486bf97046b249cc
JiangLong2019 <57093988+JiangLong2019@users.noreply.github.com>
2024-11-12 15:55:25 +0900

    Add mobile image for image with link block  (#278)

--- blocks/image-with-link/image-with-link.js
+++ blocks/image-with-link/image-with-link.js
@@ -1,17 +1,32 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 import { createElement } from '../../scripts/blocks-utils.js';
 
-export default async function decorate(block) {
-  const img = block.querySelector('picture > img');
-  const link = block.querySelector('a');
-  block.textContent = '';
-  const optimizedPic = createOptimizedPicture(img.src, 'image alt', false, [{ width: '768' }]);
+function addLink(link, optimizedPic) {
   if (link) {
     const aHref = createElement('a', '');
     aHref.href = link.href;
     aHref.append(optimizedPic);
-    block.append(aHref);
-  } else {
-    block.append(optimizedPic);
+    return aHref;
+  }
+  return optimizedPic;
+}
+
+export default async function decorate(block) {
+  const [desktopPic, mobilePic, imageLink] = block.querySelectorAll(':scope > div');
+  block.textContent = '';
+  const link = imageLink.querySelector('a');
+  const desktopImg = desktopPic.querySelector('picture > img');
+  if (desktopImg) {
+    const optimizedDesktopPic = createOptimizedPicture(desktopImg.src, 'desktop quote image', false, [{ width: '300' }]);
+    optimizedDesktopPic.classList.add('desktop-image');
+    const imageContent = addLink(link, optimizedDesktopPic);
+    block.append(imageContent);
+  }
+  const mobileImg = mobilePic.querySelector('picture > img');
+  if (mobileImg) {
+    const optimizedMobilePic = createOptimizedPicture(mobileImg.src, 'mobile quote image', false, [{ width: '300' }]);
+    optimizedMobilePic.classList.add('mobile-image');
+    const imageContent = addLink(link, optimizedMobilePic);
+    block.append(imageContent);
   }
 }
