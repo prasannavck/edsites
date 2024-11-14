43810423d5dbc4c55fa8622b556de0c33454ad68
JiangLong2019 <57093988+JiangLong2019@users.noreply.github.com>
2024-11-14 20:35:39 +0900

    Enhance callout block to add link to the number (#289)

--- blocks/callout/callout.js
+++ blocks/callout/callout.js
@@ -1,4 +1,8 @@
 export default async function decorate(block) {
   const info = block.children[1].querySelector('div');
   info.classList.add('info');
+  const aLinks = block.querySelectorAll('a.button');
+  [...aLinks].forEach((link) => {
+    link.classList.remove('button');
+  });
 }
