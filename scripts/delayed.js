8f4c4fa853f574f712449cc8b37975078119d17a
JiangLong2019 <57093988+JiangLong2019@users.noreply.github.com>
2024-11-15 22:55:23 +0900

    Show prod BV reviews on Terri Scheer and EDS main domains (#302)

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -4,7 +4,7 @@ import { getEnvType, addBazaarVoiceReviewsScript } from './blocks-utils.js';
 const BV_SCRIPT = 'https://display.ugc.bazaarvoice.com/bvstaging/static/terrischeer/en_AU/bvapi.js';
 
 async function loadBazaarVoiceScript() {
-  const scriptSrc = BV_SCRIPT;
+  let scriptSrc = BV_SCRIPT;
   const envType = getEnvType();
   if (envType === 'prod' || envType === 'live') scriptSrc = scriptSrc.replace('/bvstaging', '');
   await loadScript(scriptSrc);
